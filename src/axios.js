39ea7625431d2fa8ef80b1abd9d00120579423cc
NTMi2k3 <MiNTT.B21CN518@stu.ptit.edu.vn>
2024-06-03 10:48:25 +0700

    Remove unwanted files

--- src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-//console.log(process.env.BACKEND_URL);
-const requestax = axios.create({
-    baseURL: process.env.BACKEND_URL+'/api',
-});
-
-export default requestax;
--- src/customer/layout/index.js
+++
@@ -1,1 +0,0 @@
-export {default as layoutCustomer} from './Layout';
